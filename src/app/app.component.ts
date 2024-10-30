bbce1ae85bd0c52d753bdb9094588c42679cd3a3
Arman Ansari <you@example.com>
2024-10-30 13:33:31 +0530

    allChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular17_login_signup_localstorage';
+  title = 'my_porfolio';
 }
